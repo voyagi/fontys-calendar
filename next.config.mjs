b1bd693bb39fd310cd8a3082e86dc895be197125
voyagi <eagi333@gmail.com>
2025-01-19 15:45:26 +0100

    Updated webpack config for FullCalendar

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@fullcalendar/core': '@fullcalendar/core/index.js',
+        '@fullcalendar/react': '@fullcalendar/react/index.js',
+        '@fullcalendar/daygrid': '@fullcalendar/daygrid/index.js',
+        '@fullcalendar/timegrid': '@fullcalendar/timegrid/index.js',
+        '@fullcalendar/list': '@fullcalendar/list/index.js',
+        '@fullcalendar/interaction': '@fullcalendar/interaction/index.js',
+      }
+      return config
+    },
     transpilePackages: [
       '@fullcalendar/core',
       '@fullcalendar/react',
@@ -8,8 +20,6 @@ const nextConfig = {
       '@fullcalendar/list',
       '@fullcalendar/interaction'
     ],
-    reactStrictMode: true,
-    swcMinify: true,
   }
   
   export default nextConfig
